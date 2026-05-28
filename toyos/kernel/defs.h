776982611eabaa546edb30b66162eff72e950af0
xixioyue <213241090@seu.edu.cn>
2026-05-28 15:21:03 +0800

    lab3.3: define user memory layout and trapframe

--- toyos/kernel/defs.h
+++ toyos/kernel/defs.h
@@ -1,4 +1,5 @@
 #include "kernel/types.h"
+#include "kernel/proc.h"
 
 int sbi_call(uint64 which, uint64 arg0, uint64 arg1, uint64 arg2);
 void console_putc(int c);
---
+++ toyos/kernel/memlayout.h
@@ -0,0 +1,14 @@
+// QEMU virt + OpenSBI loads our kernel at 0x80200000.
+#define KERNBASE 0x80200000L
+
+// Keep the first user program away from the kernel image.
+#define USER_BASE 0x80400000L
+
+// ToyOS currently runs with 128 MiB of RAM.
+#define PHYSTOP (0x80000000L + 128 * 1024 * 1024)
+
+#define PGSIZE 4096
+#define TRAPFRAME_BASE (PHYSTOP - PGSIZE)
+
+#define SSTATUS_SPP (1L << 8)
+#define SSTATUS_SPIE (1L << 5)
